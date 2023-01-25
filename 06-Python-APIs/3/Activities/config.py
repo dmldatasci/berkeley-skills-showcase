c8d7cc7076bb1b1aad69aa2310edbe0d26479e51
David Levy <dmldatasci@gmail.com>
2023-01-24 20:13:30 -0700

    Added new files from Units 6, 7 and 8.

---
+++ 06-Python-APIs/3/Activities/config.py
@@ -0,0 +1,8 @@
+#  Add your OMDB API key
+omdb_api_key = "YOUR_KEY_HERE"
+nyt_api_key = "YOUR_KEY_HERE"
+weather_api_key = "YOUR_KEY_HERE"
+
+gkey = "YOUR_KEY_HERE"
+google_api_key = "YOUR_KEY_HERE"
+census_api_key = "YOUR_KEY_HERE"
